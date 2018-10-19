9ccda30fcec9f7adaa295876f49ab62dbce3092a
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2018-10-18 22:01:24 -0300

    Blindando as negociações dentro da lista

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -15,8 +15,6 @@ class NegociacaoController {
         this._listaNegociacoes.adiciona(this._criaNegociacao());
 
         this._limpaFormulario();
-
-        console.log(this._listaNegociacoes.negociacoes);
     }
 
     _criaNegociacao() {
